74b315f79990d204b07e61249a5e134e73308dc5
Douglas Maringa <douglasmaringa4@gmail.com>
2021-03-18 10:41:22 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View,StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import HomeScreen from "./screens/HomeScreen"
+import CategoriesScreen from "./screens/CategoryScreen"
+import SingleScreen from "./screens/SingleScreen"
+import CartScreen from "./screens/CartScreen"
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
-export default function App() {
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+//you first create stacks and put the screens for each stack. 
+function HomeStack (){
+//if you wanna add more routes to home page just add more screens to the home stack.
+  return(
+    <Stack.Navigator initialRouteName="Home">
+      {/*Initial is so that the all screens arent displayed at once it displays a single screen*/}
+    <Stack.Screen name="Home" component={HomeScreen}/>
+    <Stack.Screen name="single" component={SingleScreen}/>
+    </Stack.Navigator>
+  )
+}
+function CategoryStack (){
+  return(
+    <Stack.Navigator initialRouteName="Category">
+    <Stack.Screen name="Category" component={CategoriesScreen}/>
+    </Stack.Navigator>
+  )
+}
+function CartStack (){
+  return(
+    <Stack.Navigator initialRouteName="Cart">
+    <Stack.Screen name="Cart" component={CartScreen}/>
+    </Stack.Navigator>
+  )
+}
+
+//My tabs is the bottom nav bar it will hold all the stacks 
+//The home tab will hold the homestack so if youre in the categories tab you can access the home stack.
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={HomeStack} 
+       options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={"#42EE0C"} size={size} />
+        ),
+      }}/>
+       <Tab.Screen name="Categories" component={CategoryStack} 
+      options={{
+        tabBarLabel: 'Categories',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="grid" color={color} size={size} />
+        ),
+      }}/>
+       
+      <Tab.Screen name="Cart" component={CartStack} 
+      options={{
+        tabBarLabel: 'Cart',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="cart" color={color} size={size} />
+        ),
+      }}/>
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//here we will now export our nav bar which has a stack inside it.
+//sometimes it will also has a drawer.
+export default function App() {
+  return (
+    <NavigationContainer>
+       <StateProvider initialState={initialState} reducer={reducer}>
+      <MyTabs />
+      </StateProvider>
+    </NavigationContainer>
+  );
+}
